0785e711de991bbf57909d2bf340eef32712197e
Clément Schmouker <clement.schmouker@gmail.com>
2016-07-05 18:02:46 +0200

    Fixed all bugs

--- _js/main.js
+++ _js/main.js
@@ -1,6 +1,9 @@
 var cs_DOMElement = 0;
 var bMoving = false;
-var speed = 1000;
+var speed = 300;
+
+var distance = 0;
+
 for(var i = 0; i < document.querySelectorAll("a").length; i += 1) {
   document.querySelectorAll("a[href*='#']:not([href='#'])")[i].addEventListener("click", function(e) { // Will select anchor links only
     checkClicks(e); //Time in ms
@@ -12,24 +15,19 @@ element.preventDefault();
   if(bMoving == false) {
     bMoving = true;
     cs_DOMElement = document.getElementById(element.target.getAttribute("href").substring(1));
+    distance = cs_DOMElement.offsetTop - window.pageYOffset;
     scrollSmooth();
   }
 }
 
 function scrollSmooth() {
-  var distance = cs_DOMElement.offsetTop - window.pageYOffset;
-  if(window.pageYOffset < cs_DOMElement.offsetTop) {
+  var distanceLeft = cs_DOMElement.offsetTop - window.pageYOffset;
+  if(distanceLeft < -10 || distanceLeft > 10) {
     window.scrollBy(0, distance/(speed/16));
-    console.log( window.pageYOffset - cs_DOMElement.offsetTop );
-    requestAnimationFrame(scrollSmooth);
-  }
-  else if (window.pageYOffset > cs_DOMElement.offsetTop) {
-    window.scrollBy(0, distance/(speed/16));
-    console.log( window.pageYOffset + " -- " + cs_DOMElement.offsetTop );
     requestAnimationFrame(scrollSmooth);
   }
   else {
-    console.log("finished");
+    window.scrollTo(0, cs_DOMElement.offsetTop);
     bMoving = false;
     return false;
   }
