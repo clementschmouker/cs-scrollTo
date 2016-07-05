de7a55292331d1d4fb4be6d08d777b155b05396b
Clément Schmouker <clement.schmouker@gmail.com>
2016-07-05 14:56:15 +0200

    Scroll works, but no speed yet

---
+++ _js/main.js
@@ -0,0 +1,29 @@
+var cs_DOMElement = 0;
+for(var i = 0; i < document.querySelectorAll("a").length; i += 1) {
+  document.querySelectorAll("a[href*='#']:not([href='#'])")[i].addEventListener("click", function(e) { // Will select anchor links only
+    checkClicks(e, 300); //Time in ms
+  });
+}
+
+function checkClicks(element, timeInMs) {
+    element.preventDefault();
+    cs_DOMElement = document.getElementById(element.target.getAttribute("href").substring(1));
+    scrollSmooth();
+}
+
+function scrollSmooth() {
+  if(window.pageYOffset < cs_DOMElement.offsetTop) {
+    window.scrollBy(0, 1);
+    console.log( window.pageYOffset + " -- " + cs_DOMElement.offsetTop );
+    requestAnimationFrame(scrollSmooth);
+  }
+  else if (window.pageYOffset > cs_DOMElement.offsetTop) {
+    window.scrollBy(0, -1);
+    console.log( window.pageYOffset + " -- " + cs_DOMElement.offsetTop );
+    requestAnimationFrame(scrollSmooth);
+  }
+  else {
+    console.log("finished");
+    return false;
+  }
+}
